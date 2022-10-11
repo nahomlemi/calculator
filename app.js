16b8d266ef5b6e4d194b4a1240ca9ed455d91875
nahom <nahom lemi>
2022-10-11 18:57:28 +0300

    completed calculator with ui

--- app.js
+++ app.js
@@ -1,20 +1,61 @@
-//add substract multiply 
+let oprArr = {'+':'+', '-':'-', '/':'/','*':'*'};
+input = '';
+let opr = [];
+let num = [];
+let temp = '';
+let result = '';
 
-let left = '';
-let right = '';
+function calculate(){
+    for(let i = 0;i < input.length; i++){
+         if(input[i] in oprArr){
+            opr.push(input[i]);
+            num.push(temp);
+            temp = '';
+        }else{
+        temp +=input[i];
+        }
+    };
+    num.push(temp);
+    result = num[0]
+    for(i in opr){
+        if (opr[i] == '+'){
+            result = parseInt(result)+parseInt(num[parseInt(i)+1]);
+        }else if (opr[i] == '-'){
+            result = parseInt(result)-parseInt(num[parseInt(i)+1]);
+        }else if (opr[i] == '*'){
+            result = parseInt(result)*parseInt(num[parseInt(i)+1]);
+        }else if (opr[i] == '/'){
+            result = parseInt(result)/parseInt(num[parseInt(i)+1]);
+        }
+    }
+    return result;
+};
 
-function calculator(num1, opr, num2){
-    if(opr =='+'){
-        return num1+num2;
-    }else if(opr === '-'){
-        return num1-num2;
-    }else if(opr == '*'){
-        return num1*num2;
-    }else if(opr == '*'){
-        return num1*num2;
-    }else if(opr == '/'){
-        return num1/num2;
-    }else{return 'invalid operator'}
-}
+let output = document.querySelector('.output');
+let keysArray = document.querySelectorAll('.button');
 
-console.log(calculator(240,'/',12));
+Array.from(keysArray).forEach(function(key){
+    key.addEventListener('click', function(){
+        if(key.textContent == '='){
+            if(calculate() =='Infinity'){
+                alert('can not divide by zero');
+            }else{output.textContent = Math.round(calculate())};
+        }else if(key.textContent =='clear'){
+            output.textContent = '';
+            input = '';
+            for(let i =0;i<=num.length;i++){
+                num.pop();
+            }
+            console.log(num);
+            for(let i = 0;i<=opr.length;i++){
+                opr.pop();
+            }
+            temp='';
+            result = '';
+            console.log(opr)
+        }else{
+            input +=key.textContent;
+            output.textContent +=key.textContent;
+        }
+    })
+})
